5d5f3a4ffccd7bea9c196de1cba7e94793b8bd50
DkRuzzzz <jesus5gen@gmail.com>
2026-02-19 22:24:44 -0600

    Add files via upload

---
+++ Practica_TCP/Ejemplos/Ejemplo01/src/TCPClient.java
@@ -0,0 +1,39 @@
+import java.net.*;
+import java.io.*;
+
+public class TCPClient{
+    public static void main(String args[]){
+        //Argumentos: Mensaje y nombre del servidor
+        Socket s = null;
+        try{
+
+            String serverHost = args[0];
+            int serverPort = 7896;
+
+            s = new Socket(serverHost, serverPort);
+
+            DataInputStream in = new DataInputStream(s.getInputStream());
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+            BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+
+            out.writeUTF("Solicito conversión");
+
+            System.out.println(in.readUTF());
+
+            int opcion = Integer.parseInt(teclado.readLine());
+            out.writeInt(opcion);
+
+            System.out.println(in.readUTF());
+
+            double grados = Double.parseDouble(teclado.readLine());
+            out.writeDouble(grados);
+
+            System.out.println(in.readUTF());
+
+            s.close();
+
+        }catch(UnknownHostException e){System.out.println("sock:" + e.getMessage());
+        }catch(EOFException e){System.out.println("EOF:" + e.getMessage());
+        }catch(IOException e){System.out.println("IO:" + e.getMessage());}
+    }
+}
